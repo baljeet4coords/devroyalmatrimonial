9919e4b00517f5433d1aebdcd477587760a64d30
Sudhanshu Singh <s.sudhanshu@hotmail.com>
2022-12-24 15:10:36 +0530

    added nav type

--- components/Header/Navoptions.tsx
+++ components/Header/Navoptions.tsx
@@ -3,67 +3,62 @@ import { NavDropdown } from "react-bootstrap";
 import { useState } from "react";
 import { navOptionsList } from "./NavoptionLinks";
 import classes from "./Navoptions.module.scss";
-
+import { NavOptions as NavOptionsType, SubCategory } from "./types";
 
 const NavOptions: React.FC = () => {
-  
-const [show, setShow] = useState(false);
-const [navState, setNavState] = useState<any[]>([]);
+  const [show, setShow] = useState(false);
+  const [navState, setNavState] = useState<SubCategory[]>([]);
 
-const showDropdown = () =>{
+  const showDropdown = () => {
     setShow(!show);
-}
-const hideDropdown = () => {
+  };
+  const hideDropdown = () => {
     setShow(false);
-}
+  };
 
-const getLinkValue = (val:any) => {
-  const newImageUrls: any = [];
-  navOptionsList.map((linkVal) => {
-    if(linkVal.category === val)
-    linkVal.subCategory.map((subVal) => {
-      newImageUrls.push(subVal);
-      setNavState(newImageUrls);
-    })   
-  })
-}
-    return (
-        <>
-         <NavDropdown 
-         title="BROWSE PROFILES BY" 
-         menuVariant="dark"
-         show={show}
-         onMouseEnter={showDropdown} 
-         onMouseLeave={hideDropdown}
-         >
-           {navOptionsList.map((item) => {
-                if (item.subCategory.length !== 0) {
-                  return (
-                    <NavDropdown.Item
-                      key={item.category}
-                      className={classes.dropdownItem}
-                      onMouseEnter={() => getLinkValue(item.category)}
-                    >
-                      <NavDropdown.ItemText>{item.category}</NavDropdown.ItemText> 
-                    </NavDropdown.Item>
-                  );
-                } 
-              })}
-              <div className={classes.sideBar}>
-                {navState.map((item) => {
-                  return(
-                 
-                    <a href={item.uri}
-                    key={item.title}
-                    >{item.title}</a>
-                  
-                  )
-                })}
-              </div>
-        </NavDropdown>
-        </>
-    )
-}
+  const getLinkValue = (val: string) => {
+    const newSubCategory: SubCategory[] = [];
+    navOptionsList.map((linkVal: NavOptionsType) => {
+      if (linkVal.category === val)
+        linkVal.subCategory.map((subVal: SubCategory) => {
+          newSubCategory.push(subVal);
+          setNavState(newSubCategory);
+        });
+    });
+  };
+  return (
+    <>
+      <NavDropdown
+        title="BROWSE PROFILES BY"
+        menuVariant="dark"
+        show={show}
+        onMouseEnter={showDropdown}
+        onMouseLeave={hideDropdown}
+      >
+        {navOptionsList.map((item) => {
+          if (item.subCategory.length !== 0) {
+            return (
+              <NavDropdown.Item
+                key={item.category}
+                className={classes.dropdownItem}
+                onMouseEnter={() => getLinkValue(item.category)}
+              >
+                <NavDropdown.ItemText>{item.category}</NavDropdown.ItemText>
+              </NavDropdown.Item>
+            );
+          }
+        })}
+        <div className={classes.sideBar}>
+          {navState.map((item) => {
+            return (
+              <a href={item.uri} key={item.title}>
+                {item.title}
+              </a>
+            );
+          })}
+        </div>
+      </NavDropdown>
+    </>
+  );
+};
 export default NavOptions;
-
-
