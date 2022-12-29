c503baa585a7c8ed1bc365ff673fab6d5e297341
Abhinav <abhinav0939@gmail.com>
2022-12-30 02:12:50 +0530

    landing Page and media query fixes

--- components/BrowserLink/BrowserLinks.tsx
+++ components/BrowserLink/BrowserLinks.tsx
@@ -1,19 +1,54 @@
-import Nav from 'react-bootstrap/Nav';
+import React, {useState, } from "react";
+import { navOptionsList } from '../Header/NavoptionLinks';
+import classes from "./Browser.module.scss";
+import { NavOptions as NavOptionsType, SubCategory } from "../Header/types";
 
 const BrowserLink = () => {
+  const [show, setShow] = useState(false);
+  const [navState, setNavState] = useState<SubCategory[]>([]);
+
+  const showDropdown = () => {
+    setShow(!show);
+  };
+  const hideDropdown = () => {
+    setShow(false);
+  };
+
+  const getLinkValue = (val: string) => {
+    const newSubCategory: SubCategory[] = [];
+    navOptionsList.map((linkVal: NavOptionsType) => {
+      if (linkVal.category === val)
+        linkVal.subCategory.map((subVal: SubCategory) => {
+          newSubCategory.push(subVal);
+          setNavState(newSubCategory);
+        });
+    });
+  };
+    
   return (
-    <Nav fill variant="tabs" defaultActiveKey="/home">
-      <Nav.Item>
-        <Nav.Link eventKey="link-1">Loooonger NavLink</Nav.Link>
-        <h1>gssg</h1>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-2">Link</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-3">Link</Nav.Link>
-      </Nav.Item>
-    </Nav>
+    <React.Fragment>
+        <div className={classes.browserTopBox}>
+        {navOptionsList.map((item) => {
+        if (item.subCategory.length !== 0) {
+            return (
+                <span key={item.category} 
+            // onClick={showDropdown}
+            onClick={() => [getLinkValue(item.category), showDropdown()]}
+            >{item.category}</span>
+            );
+            }
+        })} 
+        </div>
+        <div className={classes.browserBottomBox}>
+        {navState.map((item) => {
+            return (
+              <a href={item.uri} key={item.title}>
+                {item.title}
+              </a>
+            );
+          })}
+        </div>
+    </React.Fragment>
   );
 }
 
--- pages/LandingPage/LandingPage.tsx
+++ pages/LandingPage/LandingPage.tsx
@@ -156,7 +156,7 @@ const LandingPage: React.FC = () => {
             <p>Jeevansathi.com is one of the leading and most trusted matrimony websites in India. Making happy marriages happen since 1998, Jeevansathi understands the importance of choosing the right partner for marriage, especially in the Indian cultural setup. It believes in providing the most secure and convenient matchmaking experience to all its members by ensuring 100% screening,exclusive privacy options, photo protection features and verification of phone numbers and more information. While the online matrimonial site connects millions of people directly, Jeevansathi also maintains a dedicated Customer Care team and offers offline Match Point Centers across the country, for deeper and personal interaction with prospective brides, grooms and /or families.</p>
             <p>Please note: Jeevansathi is only meant for users with a bonafide intent to enter into a matrimonial alliance and is not meant for users interested in dating only. Jeevansathi platform should not be used to post any obscene material, such actions may lead to permanent deletion of the profile used to upload such content.</p>
             </Row>
-            <Row className={classes.Home_white_body}>
+            <Row className={`${classes.Home_white_body} text-center`}>
             <span>BROWSE</span>
             <h1><strong>Matrimonial</strong>Profiles by</h1>
             <BrowserLink/>
