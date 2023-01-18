7e63a8b26792bd37a00c8561a5a90997d7c3a09b
Sudhanshu Singh <s.sudhanshu@hotmail.com>
2023-01-18 16:11:55 +0530

    added icons and content and images to landing page

--- components/Cards/Cards.tsx
+++ components/Cards/Cards.tsx
@@ -1,13 +1,11 @@
-import classes from "./Card.module.scss"
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
-import CustomButton from '../Button/CustomButton';
+import classes from "./Card.module.scss";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import CustomButton from "../Button/CustomButton";
 
-
-const HomeCard = (props:any) => {
-let items = props.onTitle.subCategory;
-let padd = props.onlistHeight;
-
+const HomeCard = (props: any) => {
+  let items = props.onTitle.subCategory;
+  let padd = props.onlistHeight;
 
   return (
     <Card className={classes.card}>
@@ -15,17 +13,23 @@ let padd = props.onlistHeight;
         <Card.Title>{props.onTitle.category}</Card.Title>
       </Card.Body>
       <ListGroup className="list-group-flush">
-        {items.map((val:string) => {
-          return(
-            <ListGroup.Item key={val} style={{paddingBottom:`${padd}rem`}}>{val}</ListGroup.Item>
-          )
+        {items.map((val: string) => {
+          return (
+            <ListGroup.Item key={val} style={{ paddingBottom: `${padd}rem` }}>
+              {val}
+            </ListGroup.Item>
+          );
         })}
       </ListGroup>
       <Card.Body>
-      <CustomButton onClick={() => console.log("tab")}>Get Started</CustomButton>
+        <CustomButton onClick={() => console.log("tab")}>
+          {props.onTitle.category === "Free"
+            ? "Register For Free"
+            : "Know more membership packages today"}
+        </CustomButton>
       </Card.Body>
     </Card>
   );
-}
+};
 
 export default HomeCard;
