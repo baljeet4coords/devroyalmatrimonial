3f9356f87b02bcc4967ecb1afba87537f2cbb937
Sudhanshu Singh <36724360+1SudhanshuSingh@users.noreply.github.com>
2023-04-12 21:55:22 +0530

    Update reducer.ts

--- ducks/myProfile/reducer.ts
+++ ducks/myProfile/reducer.ts
@@ -38,29 +38,29 @@ const initialState: myProfileState = {
   isLoading: false,
   response: {
     step1: {
-      output: 0;
-      message: "";
-      jsonResponse: null;
+      output: 0,
+      message: "",
+      jsonResponse: null,
     };
     step2: {
-      output: 0;
-      message: "";
-      jsonResponse: null;
+      output: 0,
+      message: "",
+      jsonResponse: null,
     };
     step3: {
-      output: 0;
-      message: "";
-      jsonResponse: null;
+      output: 0,
+      message: "",
+      jsonResponse: null,
     };
     step4: {
-      output: 0;
-      message: "";
-      jsonResponse: null;
+      output: 0,
+      message: "",
+      jsonResponse: null,
     };
     step5: {
-      output: 0;
-      message: "";
-      jsonResponse: null;
+      output: 0,
+      message: "",
+      jsonResponse: null,
     };
   };
   error: null,
