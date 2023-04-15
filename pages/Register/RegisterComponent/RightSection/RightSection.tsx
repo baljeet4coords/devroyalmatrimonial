8288a3cc27a4661a2ded104c13987e895628a0de
Sudhanshu Singh <s.sudhanshu@hotmail.com>
2023-04-15 19:24:33 +0530

    pb

--- pages/Register/RegisterComponent/RightSection/RightSection.tsx
+++ pages/Register/RegisterComponent/RightSection/RightSection.tsx
@@ -3,6 +3,7 @@ import classes from "./RightSection.module.scss";
 const RightSection: React.FC = () => {
   return (
     <Col sm={12} md={2} className={classes.right_section}>
+      <Progress type="circle" strokeWidth={3} percent={70} />
       <h3>WHY REGISTER</h3>
       <i className={classes.icon1}></i>
       <span>Lakhs of Genuine Profiles</span>
