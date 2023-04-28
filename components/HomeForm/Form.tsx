69ac6e8c5b8767481d516e3d4c116bdf37f535ee
unknown <baljit.singh@royalmatrimonial.com>
2023-04-28 18:01:50 +0530

    otp verifications ui

--- components/HomeForm/Form.tsx
+++ components/HomeForm/Form.tsx
@@ -7,6 +7,7 @@ import { SignUpType } from "../../types/authentication";
 import { AiFillEye } from "react-icons/ai";
 import { useState } from "react";
 import { callingCodes } from "../../utils/countryCodesList";
+import OTP_modal from "./OTP_modal";
 
 export interface SignUpForm {
   onSubmitForm: (values: SignUpType) => void;
@@ -15,8 +16,13 @@ export interface SignUpForm {
 
 const HomeForm: React.FC<SignUpForm> = ({ onSubmitForm, error }) => {
   const [showPassword, setShowPassword] = useState(false);
-  const [loginSpiner, setloginSpiner] = useState(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [loadingSpiner, setloadingSpiner] = useState(false);
 
+
+  function setIsOpenHandler(value: boolean) {
+    setIsOpen(value)
+  }
   const formik = useFormik({
     initialValues: {
       emailid: "",
@@ -25,125 +31,128 @@ const HomeForm: React.FC<SignUpForm> = ({ onSubmitForm, error }) => {
       countryCode: "+91",
     },
     validationSchema: SignupSchema,
-    onSubmit: (values) => {
-      setloginSpiner(true);
-      onSubmitForm(values);
+    onSubmit: () => {
+      setIsOpen(true)
     },
   });
 
+  const fromSubmit = () => {
+    setloadingSpiner(true)
+    const formValue = formik.values;
+    onSubmitForm(formValue);
+  }
+
+
+
   return (
-    <Form
-      className={`${classes.Form_Wrapper} ms-auto`}
-      onSubmit={formik.handleSubmit}
-    >
-      <Form.Group className="mb-2">
-        <Form.Label>Email</Form.Label>
-        <Form.Control
-          type="email"
-          name="emailid"
-          placeholder="Enter email"
-          className={classes.Form_input}
-          onBlur={formik.handleBlur}
-          onChange={formik.handleChange}
-        />
-        {formik.touched.emailid && formik.errors.emailid ? (
-          <div className="pt-1">
-            <Errors error={formik.errors.emailid} />
-          </div>
-        ) : null}
-      </Form.Group>
-
-      <Form.Group className="mb-2">
-        <Form.Label>Phone Number</Form.Label>
-        <Row>
-          <Col xs={3} className="mx-0 pe-0">
-            <Form.Select
-              className={classes.Form_input}
-              name="countryCode"
-              onChange={formik.handleChange}
-              defaultValue={formik.initialValues.countryCode}
-            >
-              {callingCodes.map(
-                (codes: { countryName: any; callingCode: any }) => {
-                  return (
-                    <option value={codes.callingCode} key={codes.countryName}>
-                      {codes.callingCode}
-                    </option>
-                  );
-                }
-              )}
-            </Form.Select>
-          </Col>
-          <Col xs={9} className="mx-0 ps-0">
-            <Form.Control
-              type="tel"
-              name="mobile"
-              placeholder="Enter Number"
-              className={classes.Form_input}
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
-              maxLength={formik.values.countryCode === "+91" ? 10 : 15}
-            />
-          </Col>
-        </Row>
-        {formik.touched.mobile && formik.errors.mobile ? (
-          <div className="pt-1">
-            <Errors error={formik.errors.mobile} />
-          </div>
-        ) : null}
-      </Form.Group>
-
-      <Form.Group
-        className={classes.passwordWraper}
-        controlId="formBasicPassword"
+    <>
+      <Form
+        className={`${classes.Form_Wrapper} ms-auto`}
+        onSubmit={formik.handleSubmit}
       >
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type={showPassword ? "text" : "password"}
-          name="password"
-          placeholder="Password"
-          className={classes.Form_input}
-          onBlur={formik.handleBlur}
-          onChange={formik.handleChange}
-        />
-        <AiFillEye
-          className={
-            formik.touched.password && formik.errors.password
-              ? classes.PasswordShowAline
-              : showPassword
-                ? classes.PasswordShow
-                : ""
-          }
-          onClick={() => setShowPassword(!showPassword)}
-        />
-        {formik.touched.password && formik.errors.password ? (
-          <div className="pt-1">
-            <Errors error={formik.errors.password} />
-          </div>
-        ) : null}
-      </Form.Group>
-      {error && <Errors error={error} />}
-      <Button
-        variant="danger"
-        type="submit"
-        className={`${classes.Form_btn} mt-2 w-100`}
-        disabled={!formik.isValid}
-      >
-        {loginSpiner && (
-          <Spinner
-            className={classes.loginSpiner}
-            animation="border"
-            variant="light"
+        <Form.Group className="mb-2">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            name="emailid"
+            placeholder="Enter email"
+            className={classes.Form_input}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
           />
-        )}
-        Register
-      </Button>
-      <Form.Label className="mt-4">
-        By clicking on Register Free, you confirm that you
-        accept the <span className={classes.redF12}> Terms of Use </span> and{" "}
-        <span className={classes.redF12}> Privacy Policy</span>
-      </Form.Label>
-    </Form>
+          {formik.touched.emailid && formik.errors.emailid ? (
+            <div className="pt-1">
+              <Errors error={formik.errors.emailid} />
+            </div>
+          ) : null}
+        </Form.Group>
+
+        <Form.Group className="mb-2">
+          <Form.Label>Phone Number</Form.Label>
+          <Row>
+            <Col xs={3} className="mx-0 pe-0">
+              <Form.Select
+                className={classes.Form_input}
+                name="countryCode"
+                onChange={formik.handleChange}
+                defaultValue={formik.initialValues.countryCode}
+              >
+                {callingCodes.map(
+                  (codes: { countryName: any; callingCode: any }) => {
+                    return (
+                      <option value={codes.callingCode} key={codes.countryName}>
+                        {codes.callingCode}
+                      </option>
+                    );
+                  }
+                )}
+              </Form.Select>
+            </Col>
+            <Col xs={9} className="mx-0 ps-0">
+              <Form.Control
+                type="tel"
+                name="mobile"
+                placeholder="Enter Number"
+                className={classes.Form_input}
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                maxLength={formik.values.countryCode === "+91" ? 10 : 15}
+              />
+            </Col>
+          </Row>
+          {formik.touched.mobile && formik.errors.mobile ? (
+            <div className="pt-1">
+              <Errors error={formik.errors.mobile} />
+            </div>
+          ) : null}
+        </Form.Group>
+
+        <Form.Group
+          className={classes.passwordWraper}
+          controlId="formBasicPassword"
+        >
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type={showPassword ? "text" : "password"}
+            name="password"
+            placeholder="Password"
+            className={classes.Form_input}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          <AiFillEye
+            className={
+              formik.touched.password && formik.errors.password
+                ? classes.PasswordShowAline
+                : showPassword
+                  ? classes.PasswordShow
+                  : ""
+            }
+            onClick={() => setShowPassword(!showPassword)}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <div className="pt-1">
+              <Errors error={formik.errors.password} />
+            </div>
+          ) : null}
+        </Form.Group>
+        {error && <Errors error={error} />}
+        <Button
+          variant="danger"
+          type="submit"
+          className={`${classes.Form_btn} mt-2 w-100`}
+          disabled={!formik.isValid}
+        >
+          Get OTP
+        </Button>
+        <Form.Label className="mt-4">
+          By clicking on Register Free, you confirm that you
+          accept the <span className={classes.redF12}> Terms of Use </span> and{" "}
+          <span className={classes.redF12}> Privacy Policy</span>
+        </Form.Label>
+      </Form>
+      <OTP_modal isOpen={isOpen} setIsOpenHandler={setIsOpenHandler} fromSubmit={fromSubmit} loadingSpiner={loadingSpiner} />
+    </>
   );
 };
 
--- components/HomeForm/OTP_modal.tsx
+++ components/HomeForm/OTP_modal.tsx
@@ -2,38 +2,62 @@ import React, { useState } from 'react';
 import { Modal, Button } from 'react-rainbow-components';
 import classes from "./Form.module.scss";
 import OtpInput from 'react-otp-input';
+import { Image, Spinner } from 'react-bootstrap';
 
 
-
-interface otpModal {
+export interface SignUpForm {
     isOpen: boolean;
     setIsOpenHandler: (isOpen: boolean) => void;
+    fromSubmit: () => void;
+    loadingSpiner: boolean;
 }
 
-const OTP_modal: React.FC<otpModal> = ({ isOpen, setIsOpenHandler }) => {
+const OTP_modal: React.FC<SignUpForm> = ({ isOpen, setIsOpenHandler, fromSubmit, loadingSpiner }) => {
     const [otp, setOtp] = useState('');
 
 
+    const handleRegister = () => {
+        fromSubmit();
+    }
+
     return (
-        <div className="rainbow-m-bottom_xx-large rainbow-p-bottom_xx-large">
+        <div className=" rainbow-p-bottom_xx-large">
             <Modal
                 isOpen={isOpen}
                 onRequestClose={() => setIsOpenHandler(false)}
                 title="OTP Varification"
                 footer={
-                    <div className="rainbow-flex rainbow-justify_spread">
+                    <div className={classes.OtpBtn_Container}>
                         <Button label="Resend Otp" variant="neutral" />
-                        <Button label="Verify" variant="brand" />
+                        <Button className={classes.verify_otp} onClick={handleRegister}>
+                            {loadingSpiner && (
+                                <Spinner
+                                    className={classes.loginSpiner}
+                                    animation="border"
+                                    variant="light"
+                                />
+                            )}
+                            Verify
+                        </Button>
                     </div>
                 }
             >
                 <div className={classes.modal_Inner}>
+                    <div className={classes.modal_middle}>
+                        <div className={classes.otpImg_container}>
+                            <Image className={classes.otpImage} src="Images/sms-sending.png" alt="sendSMS" />
+                        </div>
+                        <h3>Verification</h3>
+                        <p>You will get a OTP via <span>SMS</span> on your mobile number</p>
+                    </div>
 
                     <OtpInput
                         value={otp}
                         onChange={setOtp}
                         numInputs={6}
-                        renderSeparator={<span>-</span>}
+                        containerStyle={{ gap: "0.8rem" }}
+                        inputType='text'
+                        inputStyle={{ height: "40px", width: "40px", border: "none", borderBottom: "3px solid #bdb3b3" }}
                         renderInput={(props) => <input {...props} />}
                     />
                 </div>
