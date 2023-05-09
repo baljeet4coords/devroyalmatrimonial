9e157181a0996830fa1a4e015c2cb74e72f31808
Sudhanshu Singh <s.sudhanshu@hotmail.com>
2023-05-09 13:10:27 +0530

    removed go back buttton

--- pages/DesiredProfile/index.tsx
+++ pages/DesiredProfile/index.tsx
@@ -222,13 +222,6 @@ const DesiredProfilePage: React.FC = () => {
               <Loader />
             ) : (
               <Col sm={12} className={`${classes.form_wrapper}`}>
-                <Button
-                  variant="link"
-                  className="px-0"
-                  onClick={() => router.push("/Register")}
-                >
-                  Go back to registerations
-                </Button>
                 <form className={classes.formEdit}>
                   <DoubleInput
                     data={AgeFromYearList}
